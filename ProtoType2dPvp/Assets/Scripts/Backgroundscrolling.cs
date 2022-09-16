c6229af1dbdfebdd000ab91cde2f1eb300c81965
Cyborg <leonardo.delphi@gmail.com>
2022-09-16 03:47:13 -0300

    Build upload  first animations working ,  background setup ok

---
+++ ProtoType2dPvp/Assets/Scripts/Backgroundscrolling.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Backgroundscrolling : MonoBehaviour
+{
+    Material background;
+    Vector2 offset;
+    public float xVelocity,yVelocity;
+    private void Awake(){
+        background = GetComponent<Renderer>().material;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        offset = new Vector2(xVelocity,yVelocity);               
+        background.mainTextureOffset += offset * Time.deltaTime;
+    }
+}
